c4b071fbd31175ea354a93ab43ab0931b9c69e3c
chasejenkins303 <chasejenkins303@gmail.com>
2024-10-06 23:45:23 -0400

    mobile

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,16 +4,17 @@ function Modal(props: any){
         window.open(fileURL, '_blank'); // Open file in a new tab
       };
     return (
-        <div className="fixed w-screen h-screen flex justify-center items-center bg-black bg-opacity-50 z-20">
-            <div className="w-[50vw] h-[50vh] bg-lightgreen rounded-xl">
-                <button className="absolute top-0 right-0 text-white mr-10 mt-6 p-4 text-3xl bg-black bg-opacity-50" onClick={()=>props.setUseModal(false)}> X </button>
-                <div className="flex flex-col items-center py-12 px-12">
-                    <h1 className="text-lg md:text-3xl pb-12">Confirm it is you, {props.text}</h1>
+        <div className="fixed top-0 left-0 w-[100vw] h-[100vh] lg:w-screen lg:h-screen flex justify-center items-center bg-black bg-opacity-50 z-20">
+            <div className="w-[80vw] h-[60vh] md:w-[50vw] md:h-[50vh] bg-lightgreen rounded-xl">
+                <button className="absolute top-0 right-0 text-white lg:mr-10 lg:mt-6 lg:p-4 text-3xl bg-black bg-opacity-50" onClick={()=>props.setUseModal(false)}> X </button>
+                <div className="flex flex-col items-center py-6 px-4 lg:py-12 lg:px-12">
+                    <h1 className="text-lg md:text-3xl pb-6 lg:pb-12">Confirm it is you, {props.text}</h1>
                     <p className="text-sm md:text-lg">If you click confirm, you will see the person that {props.text} is assigned to get a gift for. If you are not {props.text} please click cancel so you don't spoil the surprise!</p>
+                    {props.text == 'Sherri' ? <p className="text-xs lg:text-lg lg:pt-4">(Hey Aunt Sherri, since I know you like puzzles, somewhere on this site is a number. If you find that number, you can enter it somewhere on this site to solve the puzzle)</p> : <></>}
                 </div>
-                <div className="flex justify-center items-center gap-4">
-                    <button className="py-6 px-12 bg-red rounded-xl text-xl text-white" onClick={()=>props.setUseModal(false)}>Cancel</button>
-                    <button className="py-6 px-12 bg-green rounded-xl text-xl text-white" onClick={handleButtonClick}>Confirm</button>
+                <div className="flex justify-center items-center gap-8 lg:gap-4">
+                    <button className="p-4 lg:py-6 lg:px-12 bg-red rounded-xl text-xl text-white" onClick={()=>props.setUseModal(false)}>Cancel</button>
+                    <button className="p-4 lg:py-6 lg:px-12 bg-green rounded-xl text-xl text-white" onClick={handleButtonClick}>Confirm</button>
                 </div>
             </div>
         </div>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -21,15 +21,18 @@ function Main(){
         'JG.txt',
       ];
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-green z-5">
-          {useModal ? <Modal setUseModal={setUseModal} text={name}/> : <></>}
-          <h1 className="text-2xl text-white font-bold mb-6 z-10 bg-red p-6 rounded-xl">Select your name</h1>
-          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-12">
-            {files.map((file) => (
-               <GiftBox text={file} key={file} setUseModal={setUseModal} setName={setName}/>            
-            ))}
+        <>
+          <div className="hidden number_here"><p>0112358</p></div>
+          <div className="flex flex-col items-center justify-center min-h-screen bg-green z-5">
+            {useModal ? <Modal setUseModal={setUseModal} text={name}/> : <></>}
+            <h1 className="text-2xl text-white font-bold mb-6 z-10 bg-red p-6 rounded-xl">Select your name</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-12">
+              {files.map((file) => (
+                <GiftBox text={file} key={file} setUseModal={setUseModal} setName={setName}/>            
+              ))}
+            </div>
           </div>
-        </div>
+        </>
       );
 }
 
