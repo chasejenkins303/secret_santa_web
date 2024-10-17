5691d096bd08a6626bb27ce3dd7b08f692409456
chasejenkins303 <chasejenkins303@gmail.com>
2024-10-16 23:37:38 -0400

    puzzle instructions update

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,7 +10,7 @@ function Modal(props: any){
                 <div className="flex flex-col items-center py-6 px-4 lg:py-12 lg:px-12">
                     <h1 className="text-lg md:text-3xl pb-6 lg:pb-12">Confirm it is you, {props.text}</h1>
                     <p className="text-sm md:text-lg">If you click confirm, you will see the person that {props.text} is assigned to get a gift for. If you are not {props.text} please click cancel so you don't spoil the surprise!</p>
-                    {props.text == 'Sherri' ? <p className="text-xs lg:text-lg lg:pt-4">(Hey Aunt Sherri, since I know you like puzzles, somewhere on this site is the start of a sequence. If you find that sequence, you can then enter the NEXT number of that sequence somewhere on this site to solve the puzzle)</p> : <></>}
+                    {props.text == 'Sherri' ? <p className="text-xs lg:text-lg lg:pt-4">(Hey Aunt Sherri, since I know you like puzzles, somewhere on this site is the start of a sequence. If you find that sequence, you can then enter the NEXT number of that sequence somewhere on this site to solve the puzzle. It is NOT in anyone elses gift box by the way)</p> : <></>}
                 </div>
                 <div className="flex justify-center items-center gap-8 lg:gap-4">
                     <button className="p-4 lg:py-6 lg:px-12 bg-red rounded-xl text-xl text-white" onClick={()=>props.setUseModal(false)}>Cancel</button>
