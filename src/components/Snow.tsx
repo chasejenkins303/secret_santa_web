7dc928dee925cfd6ecf30b3693b4e0ecf07430b7
chasejenkins303 <chasejenkins303@gmail.com>
2025-12-01 15:29:53 -0500

    snow update

--- src/components/Snow.tsx
+++ src/components/Snow.tsx
@@ -3,12 +3,13 @@ import './snow.css'; // Import the custom CSS for snow
 
 function Snow() {
     return (
-        <div className="absolute top-0 left-0 w-full h-full pointer-events-none z-0">
+        <div className="fixed top-0 left-0 w-full h-full pointer-events-none z-0">
             <div className="snow w-full h-full">
                 <div className="snowfall" />
             </div>
         </div>
     );
-};
+}
+
 
 export default Snow;
